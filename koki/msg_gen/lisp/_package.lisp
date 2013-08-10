629b59961ac850dfd896872298147c34ac3c25b9
Luke Teacy <wtlteacy@soton.ac.uk>
2013-08-10 20:15:06 +0100

    removed all other unnecessary files I could find

--- koki/msg_gen/lisp/_package.lisp
+++
@@ -1,7 +0,0 @@
-(cl:defpackage koki-msg
-  (:use )
-  (:export
-   "<KOKIMSG>"
-   "KOKIMSG"
-  ))
-
--- koki/msg_gen/lisp/_package_KokiMsg.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package koki-msg)
-(cl:export '(HEADER-VAL
-          HEADER
-          TAGS-VAL
-          TAGS
-))
