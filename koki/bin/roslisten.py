d742e25c5f44dc4914f503d5cecf014bfbd77fac
jamesmcinerney <contact@jamesmc.com>
2013-08-10 15:51:34 +0100

    added some controls to roslisten

--- koki/bin/roslisten.py
+++ koki/bin/roslisten.py
@@ -8,10 +8,19 @@ import rospy
 # Import custom message data.
 from koki.msg import KokiMsg
 
+#Import drone controller
+from koki.bin.drone_controller import BasicDroneController
+
+#Initialize drone controller
+droneController = BasicDroneController()
+
 # Create a callback function for the subscriber.
 def callback(data):
     # Simply print out values in our custom message.
     rospy.logwarn(rospy.get_name() + " I heard %s", data.tags)
+    tags = list(data.tags)
+    if len(tags)>0:
+        droneController.SendLand()
 
 # This ends up being the main while loop.
 def listener():
@@ -26,5 +35,8 @@ def listener():
 if __name__ == '__main__':
     # Initialize the node and name it.
     rospy.init_node('kokilisten', anonymous = True)
+
+    droneController.SendTakeoff()
+
     # Go to the main loop.
     listener()
